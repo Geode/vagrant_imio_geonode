dee102166f6f5a0feb426d93742ecd8609f6afe0
Julien Collaer <julien.collaer@opengeode.be>
2014-12-21 11:37:56 +0100

    Update imio-geonode-1.0.sh

--- imio-geonode-1.0.sh
+++ imio-geonode-1.0.sh
@@ -151,10 +151,10 @@ sed -i 's/WSGIScriptAlias \/ \/var\/www\/geonode\/wsgi\/geonode.wsgi/WSGIScriptA
 
 a2ensite geonode
 a2dissite 000-default
-chown www-data:www-data /var/www/geonode/geonode/static/
-chown www-data:www-data /var/www/geonode/geonode/uploaded/
-mkdir /var/www/geonode/geonode/static_root/
-chown www-data:www-data /var/www/geonode/geonode/static_root/
+chown www-data:www-data /var/www/imio_geonode/imio_geonode/static/
+chown www-data:www-data /var/www/imio_geonode/imio_geonode/uploaded/
+mkdir /var/www/imio_geonode/imio_geonode/static_root/
+chown www-data:www-data /var/www/imio_geonode/imio_geonode/static_root/
 a2enmod wsgi
 a2enmod proxy_http
 
