6e74398187f26654275366af258c9bb38640c1d7
Julien Collaer <julien.collaer@opengeode.be>
2015-01-25 16:02:50 +0100

    Update addurb.py

--- setup/addurb.py
+++ setup/addurb.py
@@ -8,8 +8,9 @@ def main(options):
   cat = Catalog("http://localhost:8080/geoserver/rest", "admin", options.gpw)
   
   #create datrastore for URB schema
+  ws = cat.create_workspace(options.alias,'imio.be')
   
-  ds = cat.create_datastore(name)
+  ds = cat.create_datastore(options.alias, ws)
   ds.connection_parameters.update(
       host=options.urbanUrl,
       port="5432",
@@ -19,10 +20,31 @@ def main(options):
       dbtype="postgis")
   
   cat.save(ds)
-  ds = cat.get_store(name)
+  ds = cat.get_store(options.alias)
   
+  #config object
+  urb = {
+  	"capa":"Parcelles",
+  	"toli":"cadastre_ln_toponymiques",
+  	"canu":"cadastre_pt_num",
+  	"cabu":"Batiments",
+  	"gept":"cadastre_points_generaux",
+  	"gepn":"cadastre_pol_gen",
+  	"inpt":"point",
+  	"geli":"cadastre_ln_generales",
+  	"inli":"cadastre_ln_informations",
+  	"topt":"point",
+  	}   	
+  	
   #connect to tables and create layers and correct urban styles
-  
+  for table in urb:
+    style = urb[table]
+    ft = cat.publish_featuretype(table, ds, 'EPSG:31370', srs='EPSG:31370')
+    ft.default_style = style
+    cat.save(ft)
+    resource = ft.resource
+    resource.title = options.alias+"_"+table
+    resource.save()
   
 if __name__ == "__main__":
 	parser = OptionParser()
@@ -30,12 +52,14 @@ if __name__ == "__main__":
 	parser.add_option("-u", "--urbanUrl", action="store", type="string", dest="urbanUrl", default="", help="Urban URL [default: %default]")
 	parser.add_option("-r", "--ropw", action="store", type="string", dest="ropw", default="", help="Remote postGIS ro_user password [default: %default]")
 	parser.add_option("-d", "--database", action="store", type="string", dest="database", default="urb_xxx", help="remote urban database name [default: %default]")
-	parser.add_option("-p", "--prefix", action="store", type="string", dest="prefix", default="xxx", help="prefix alias [default: %default]")
+	parser.add_option("-p", "--alias", action="store", type="string", dest="alias", default="", help="prefix alias [default: %default]")
 	(options, args) = parser.parse_args()
 	if options.gpw is None:
-    parser.error('Admin geoserver password not given')
+    		parser.error('Admin geoserver password not given')
 	if options.urbanUrl is None:
-    parser.error('Urban postGIS URL not given')
+    		parser.error('Urban postGIS URL not given')
 	if options.ropw is None:
-    parser.error('ro_user password not given')
+    		parser.error('ro_user password not given')
+	if options.alias is None:
+    		parser.error('alias not given')
 	main(options)
